fe791c8cfc791ce492dd3266aa52797f31d71b48
Parviz Karimli <devparviz@gmail.com>
2019-10-02 12:00:10 +0400

    Added Talkshak sign-in view to login view; changed favicon, logo, and logo2 images

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,97 @@
-@extends('layouts.dashboard.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>JustIM</title>
+    <link href="dist/img/favicon.png" type="image/png" rel="icon">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/perfect-scrollbar.min.css" >
+    <link rel="stylesheet" href="dist/css/themify-icons.css">
+    <link rel="stylesheet" href="dist/css/emoji.css">
+    <link rel="stylesheet" href="dist/css/style.css" >
+    <link rel="stylesheet" href="dist/css/responsive.css" >
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+</head>
+<body class="start">
+<main>
+    <div class="layout">
+        <!-- Start of Sign In -->
+        <div class="sign-bg">
+            <div class="start">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="register-content">
+                                <div class="login-header">
+                                    <div class="logo">
+                                        <img src="dist/img/logo.png" alt="">
+                                    </div>
+                                    <h1><i class="ti-key"></i>Sign in</h1>
                                 </div>
+                                <form class="login-up" method="post">
+                                    {{ csrf_field() }}
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <input type="email" id="inputEmail" class="form-control" placeholder="Email Address" name="email" value="{{ old('email') }}" required autofocus>
+                                        <button class="btn icon"><i class="ti-email"></i></button>
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+                                        <button class="btn icon"><i class="ti-lock"></i></button>
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="checkbox">
+                                            <label style="cursor:pointer">
+                                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn button" formaction="{{ route('login') }}">Sign In</button>
+                                    <div class="form-group">
+                                        <div class="col-md-8 col-md-offset-4">
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                Forgot Your Password?
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="callout">
+                                        <span>Don't have account? <a href="{{ route('register') }}">Create Account</a></span>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
+                        <div class="col-lg-8 col-md-6">
+                            <div class="page-meta">
+                                <h2>JustIM is a simple instant messenger.</h2>
+                                <span>Join us and enjoy!</span>
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- End of Sign In -->
     </div>
-</div>
-@endsection
+</main>
+
+<script src="dist/js/jquery3.3.1.js"></script>
+<script src="dist/js/vendor/jquery-slim.min.js"></script>
+<script src="dist/js/vendor/popper.min.js"></script>
+<script src="dist/js/bootstrap.min.js"></script>
+<script src="dist/js/perfect-scrollbar.min.js"></script>
+<script src="dist/js/script.js"></script>
+</body>
+</html>
