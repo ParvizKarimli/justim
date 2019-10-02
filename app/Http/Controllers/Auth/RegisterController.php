4d4bbdaa61f91d8858cfce7ad54eed5e07539832
Parviz Karimli <devparviz@gmail.com>
2019-10-02 11:10:55 +0400

    Added username field to RegisterController@create

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,6 +65,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
