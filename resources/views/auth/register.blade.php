ab79223a362ef5d1955b78cf67cd8d9afefe5d6b
Parviz Karimli <devparviz@gmail.com>
2019-10-02 12:16:32 +0400

    Added Talkshak sign-up view to register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,91 +1,105 @@
-@extends('layouts.dashboard.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>JustIM</title>
+    <link href="dist/img/favicon.png" type="image/png" rel="icon">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/perfect-scrollbar.min.css" >
+    <link rel="stylesheet" href="dist/css/themify-icons.css">
+    <link rel="stylesheet" href="dist/css/emoji.css">
+    <link rel="stylesheet" href="dist/css/style.css" >
+    <link rel="stylesheet" href="dist/css/responsive.css">
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+</head>
+<body class="start">
+<main>
+    <div class="layout">
+        <!-- Start of Sign In -->
+        <div class="sign-bg">
+            <div class="start">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-4">
+                            <div class="register-content">
+                                <div class="login-header">
+                                    <div class="logo">
+                                        <img src="dist/img/logo.png" alt="">
+                                    </div>
+                                    <h1><i class="ti-key"></i>Sign up</h1>
+                                </div>
+                                <form class="login-up" method="post">
+                                    {{ csrf_field() }}
+                                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                        <input type="text" id="" class="form-control" placeholder="Name" name="name" value="{{ old('name') }}" required autofocus>
+                                        <button class="btn icon"><i class="ti-user"></i></button>
+                                        @if ($errors->has('name'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('name') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                                        <input type="text" id="inputuser" class="form-control" placeholder="User Name" name="username" value="{{ old('username') }}" required>
+                                        <button class="btn icon"><i class="ti-user"></i></button>
+                                        @if ($errors->has('username'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('username') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <input type="email" id="inputEmail" class="form-control" placeholder="Email Address" name="email" value="{{ old('email') }}" required>
+                                        <button class="btn icon"><i class="ti-email"></i></button>
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+                                        <button class="btn icon"><i class="ti-lock"></i></button>
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" id="" class="form-control" placeholder="Confirm Password" name="password_confirmation" required>
+                                        <button class="btn icon"><i class="ti-lock"></i></button>
+                                    </div>
+                                    <button type="submit" class="btn button" formaction="{{ route('register') }}">Sign Up</button>
+                                    <div class="callout">
+                                        <span>Already have account? <a href="{{ route('login') }}">Click Here</a></span>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <label for="username" class="col-md-4 control-label">Username</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required>
-
-                                @if ($errors->has('username'))
-                                <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="col-md-6 col-lg-8">
+                            <div class="page-meta">
+                                <h2>JustIM is a simple instant messenger.</h2>
+                                <span>Join us and enjoy!</span>
                             </div>
                         </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- End of Sign In -->
     </div>
-</div>
-@endsection
+</main>
+
+<script src="dist/js/jquery3.3.1.js"></script>
+<script src="dist/js/vendor/jquery-slim.min.js"></script>
+<script src="dist/js/vendor/popper.min.js"></script>
+<script src="dist/js/bootstrap.min.js"></script>
+<script src="dist/js/perfect-scrollbar.min.js"></script>
+<script src="dist/js/script.js"></script>
+</body>
+</html>
